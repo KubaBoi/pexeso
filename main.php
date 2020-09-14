b6fb2636f2e66b203a91e82d54470682a0ef9c7a
KubaBoi <jakubanderle@outlook.cz>
2020-09-14 21:44:43 +0200

    debug

--- main.php
+++ main.php
@@ -1,26 +1,43 @@
 <?php
 #script po zvavolani pozmeni stav hry a vrati aktualni stav
-$move1 = $_REQUEST["move1"];
-$move2 = $_REQUEST["move2"];
+$move1 = (int)$_REQUEST["move1"];
+$move2 = (int)$_REQUEST["move2"];
 $gameId = $_REQUEST["gameId"];
 $playerId = $_REQUEST["playerId"];
 
-$dataFile = fopen("game" . $gameId, "r");
-$data = fread($dataFile, filesize("game" . $gameId));
-$data = explode("|", json_encode($data));
+if (file_exists("game" . $gameId)) {
+    $dataFile = fopen("game" . $gameId, "r");
+    $data = fread($dataFile, filesize("game" . $gameId));
+    $data = explode("|", json_encode($data));
 
-if (count($data) >= 4) {
-    $player1 = $data[0];
-    $player2 = $data[1];
-    $whosPlaying = $data[2];
-    $pixels = explode(",", $data[3]);
-    $changedPixels = explode(",", $data[4]);
-    
-    #vytvoreni pole s pexesovyma pexesákama
-    for ($i = 0; $i < 64; $i++) {
-        $x = round((int)$pixels[$i] / 8, 0, PHP_ROUND_HALF_DOWN);
-        $y = (int)$pixels[$i] % 8;
+    if (count($data) >= 4) {
+        $player1 = $data[0];
+        $player2 = $data[1];
+        $whosPlaying = $data[2];
+        $pixels = explode(",", $data[3]);
+        $changedPixels = explode(",", $data[4]);
+
+        if ($move1 < 0 || $move2 < 0) {
+            $jsonObj->code = 1;
+            $jsonObj->comment = "Vraci stav pole";
+            $jsonObj->whosPlayin = $whosPlaying;
+            $jsonObj->pixels = $pixels;
+            $jsonObj->change = $changedPixels;
+        }
+        else {
+            #vytvoreni pole s pexesovyma pexesákama
+            for ($i = 0; $i < 64; $i++) {
+                $x = round((int)$pixels[$i] / 8, 0, PHP_ROUND_HALF_DOWN);
+                $y = (int)$pixels[$i] % 8;
+            }
+        }
     }
 }
+else {
+    $jsonObj->code = 0;
+    $jsonObj->comment = "Hra neexistuje: " . $e;
+}
+
+echo json_encode($jsonObj);
 
 ?>
